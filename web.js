bb99f3493b2b51aa3dd4b0f87ab8916b0cc26322
BadCoder1337 <anton.tiz.musin@gmail.com>
2019-03-18 05:48:16 +0300

    SHS

--- web.js
+++ web.js
@@ -3,7 +3,7 @@ var port = process.env.PORT || 5000;
 
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Сань хуй соси');
+  res.end('Sanya hui sosi');
 }).listen(port, function() {
   console.log("Listening on " + port);
 });
