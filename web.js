6c61db41789cc235e049dbe67756a2197bdb2ded
BadCoder1337 <anton.tiz.musin@gmail.com>
2019-03-18 05:59:28 +0300

    Update web.js

--- web.js
+++ web.js
@@ -3,8 +3,10 @@ var port = process.env.PORT || 5000;
 
 http.createServer(function (req, res) {
   res.writeHead(302, {
-  'Location': 'https://www.youtube.com/watch?v=grs8qligDWo'
-});
+    'Location': 'https://www.youtube.com/watch?v=grs8qligDWo'
+  });
+  res.end();
+  // вот фэк-то
 }).listen(port, function() {
   console.log("Listening on " + port);
 });
